7067728d83d8f61abc004b03b6f6e858e0d4768e
Nelson Gonzalez <nelson.gonzalez@grupokaizen.net>
2023-06-21 21:03:00 -0400

    Update 20230621

---
+++ core/Service/Providers/Model/ProviderModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core.Service
+{
+    public class ProviderModel
+    {
+        public int CodigoProveedor { get; set; }
+        public string RazonSocialProveedor { get; set; }
+        public int CodigoPais { get; set; }
+        public string DescripcionPais { get; set; }
+        public string Ciudad { get; set; }
+        public string Estado { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string CodigoEstadoProveedor { get; set; }
+        public string DescripcionEstadoProveedor { get; set; }
+
+        public DateTime FechaEfectividad { get; set; }
+    }
+}
---
+++ webLoyal/Controllers/ProviderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webLoyal.Controllers
+{
+    public class ProviderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
