a16c81f7209e149a88cc5d83d8119bbccfca2a23
Nelson Gonzalez <nelson.gonzalez@grupokaizen.net>
2023-07-05 13:40:16 -0400

    Update Resources language

--- webLoyal/Program.cs
+++ webLoyal/Program.cs
@@ -1,10 +1,20 @@
 using core;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
+var supportedCultures = new[]
+{
+    new CultureInfo("en-US"),
+    new CultureInfo("es"),
+    // Agrega otros idiomas compatibles aquí
+};
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.InitializerCore();
+builder.Services.AddLocalization(options => options.ResourcesPath = "/Resources/language");
+builder.Services.AddMvc().AddViewLocalization().AddDataAnnotationsLocalization();
 
 var app = builder.Build();
 
@@ -16,6 +26,12 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("en-US"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+});
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
