2a69f25d1a4594e52eff48be6873e5924e56d692
Nelson Gonzalez <nelson.gonzalez@grupokaizen.net>
2023-07-24 21:34:02 -0400

    Create Draft Views and controller

---
+++ webLoyal/Controllers/draftController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webLoyal.Controllers
+{
+    public class draftController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
