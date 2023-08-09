8d1c7e80bf51d95f82833575208a148667d5b213
Nelson Gonzalez <nelson.gonzalez@grupokaizen.net>
2023-08-09 07:21:10 -0400

    Create Agents Controller and Details View

---
+++ webLoyal/Controllers/AgentsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webLoyal.Controllers
+{
+    public class AgentsController : Controller
+    {
+    
+    [HttpGet]
+    [Route("Agents/Detail")]
+    public IActionResult Detail()
+    {
+        return View();
+    }
+    }
+}
