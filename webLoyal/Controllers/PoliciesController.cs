44718dd831ab3aa4ee44ca8967cf0252365e79c6
Nelson Gonzalez <nelson.gonzalez@grupokaizen.net>
2023-08-31 01:42:30 -0400

    Update policies

--- webLoyal/Controllers/PoliciesController.cs
+++ webLoyal/Controllers/PoliciesController.cs
@@ -5,22 +5,15 @@ namespace webLoyal.Controllers
 {
     public class PoliciesController : Controller
     {
-        public IActionResult Index()
+        private readonly IPolicies _request;
+        public PoliciesController(IPolicies request)
         {
-            var model = new List<PoliciesDetailModel>();
-            model.Add(new PoliciesDetailModel { PolicyNumber = "000-000-0001", EffectiveStartDate = "2022-01-01", PlanCode = "Liberty", CódigoTipoVenta = "Abierto", EffectiveEndDate = DateTime.Now.ToString() });
-            model.Add(new PoliciesDetailModel { PolicyNumber = "000-000-0002", EffectiveStartDate = "2022-01-01", PlanCode = "Liberty", CódigoTipoVenta = "Abierto", EffectiveEndDate = DateTime.Now.ToString() });
-            model.Add(new PoliciesDetailModel { PolicyNumber = "000-000-0003", EffectiveStartDate = "2022-01-01", PlanCode = "Liberty", CódigoTipoVenta = "Abierto", EffectiveEndDate = DateTime.Now.ToString() });
-            model.Add(new PoliciesDetailModel { PolicyNumber = "000-000-0004", EffectiveStartDate = "2022-01-01", PlanCode = "Liberty", CódigoTipoVenta = "Abierto", EffectiveEndDate = DateTime.Now.ToString() });
-            model.Add(new PoliciesDetailModel { PolicyNumber = "000-000-0005", EffectiveStartDate = "2022-01-01", PlanCode = "Liberty", CódigoTipoVenta = "Abierto", EffectiveEndDate = DateTime.Now.ToString() });
-            model.Add(new PoliciesDetailModel { PolicyNumber = "000-000-0006", EffectiveStartDate = "2022-01-01", PlanCode = "Liberty", CódigoTipoVenta = "Abierto", EffectiveEndDate = DateTime.Now.ToString() });
-            model.Add(new PoliciesDetailModel { PolicyNumber = "000-000-0007", EffectiveStartDate = "2022-01-01", PlanCode = "Liberty", CódigoTipoVenta = "Abierto", EffectiveEndDate = DateTime.Now.ToString() });
-            model.Add(new PoliciesDetailModel { PolicyNumber = "000-000-0008", EffectiveStartDate = "2022-01-01", PlanCode = "Liberty", CódigoTipoVenta = "Abierto", EffectiveEndDate = DateTime.Now.ToString() });
-            model.Add(new PoliciesDetailModel { PolicyNumber = "000-000-0009", EffectiveStartDate = "2022-01-01", PlanCode = "Liberty", CódigoTipoVenta = "Abierto", EffectiveEndDate = DateTime.Now.ToString() });
-            model.Add(new PoliciesDetailModel { PolicyNumber = "000-000-0010", EffectiveStartDate = "2022-01-01", PlanCode = "Liberty", CódigoTipoVenta = "Abierto", EffectiveEndDate = DateTime.Now.ToString() });
-            model.Add(new PoliciesDetailModel { PolicyNumber = "000-000-0011", EffectiveStartDate = "2022-01-01", PlanCode = "Liberty", CódigoTipoVenta = "Abierto", EffectiveEndDate = DateTime.Now.ToString() });
-
-            return View(model);
+            _request = request;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var x = await _request.GetAll();
+            return View(x.ToList());
         }
 
         public IActionResult Detail() { return View(); }
