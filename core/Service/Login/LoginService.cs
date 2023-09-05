7fba2e38a18c2e4ea03ec6c9cc94d5d964320444
Nelson Gonzalez <nelson.gonzalez@grupokaizen.net>
2023-09-05 08:56:36 -0400

    Update

--- core/Service/Login/LoginService.cs
+++ core/Service/Login/LoginService.cs
@@ -6,38 +6,17 @@ namespace core.Service
 {
     public class LoginService : ILogin
     {
+        private readonly IHttpServicesUnAuthorized _unAuthorized;
+        public LoginService(IHttpServicesUnAuthorized unAuthorized)
+        {
+            this._unAuthorized = unAuthorized;
+        }
         public async Task<LoginAuthModel> Login(LoginModel model)
         {
-            HttpResponseMessage respuesta;
+            var y = await _unAuthorized.PostUnAuthorizedAsync<LoginModel, ResponseRequestLoginAuthModel>("api/v1/Auth", model);
 
-            using (var httpClient = new HttpClient())
-            {
-				var url = "http://apirestloyal-env.us-east-1.elasticbeanstalk.com/api/v1/Auth";
-				respuesta = await httpClient.PostAsJsonAsync(url, model);
-			}                
+            return y.Content.User;
 
-            if (respuesta.IsSuccessStatusCode)
-            {
-                var responseAuth = await respuesta.Content.ReadFromJsonAsync<ResponseAuthApi>();
-                var loginAuthModel = new LoginAuthModel
-                {
-                    AddressEmail = responseAuth.Content.User.DireccionEmail,
-                    CodeProfile = responseAuth.Content.User.CodigoPerfil,
-                    CodeStateUser = responseAuth.Content.User.CodigoEstadoUsuario.ToString(),
-                    FullNameUser = responseAuth.Content.User.NombreCompletoUsuario,
-                    IndicatorChangePassword = Convert.ToInt32(responseAuth.Content.User.IndicadorCambioPassword), 
-                    IndicatorMailVerified = Convert.ToInt32(responseAuth.Content.User.IndicadorCorreoVerificado),
-                    IndicatorViewAgents = Convert.ToInt32(responseAuth.Content.User.IndicadorVistaAgentes),                 
-                    NamePage = responseAuth.Content.User.NombrePagina,
-                    NameProfile = responseAuth.Content.User.NombrePerfil,
-                    UserCode = responseAuth.Content.User.CodigoUsuario                               
-				};
-				return loginAuthModel;
-			}
-            else
-            {
-                throw new Exception("Autenticación fallida");
-			}
-		}
+        }
     }
 }
