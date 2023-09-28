d9b7be9b1bbf6c3390f22bf5bc518cdd5501f352
Nelson Gonzalez <nelson.gonzalez@grupokaizen.net>
2023-09-27 20:47:24 -0400

    uPDATE 20230926

--- core/Service/Lives/Model/CifrasPersonasCertificadoResponseModel.cs
+++ core/Service/Lives/Model/CifrasPersonasCertificadoResponseModel.cs
@@ -8,13 +8,83 @@
         public int MenoresHombres { get; set; }
         public int MenoresMujeres { get; set; }
 
+        public double PorcentajeHombresTotales
+        {
+            get
+            {
+                if (TotalPersonas != 0)
+                {
+                    return ((double)AdultosHombres+ (double)MenoresHombres) / TotalPersonas * 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }        
+        public double PorcentajeMujeresTotales
+        {
+            get
+            {
+                if (TotalPersonas != 0)
+                {
+                    return ((double)AdultosMujeres+ (double)MenoresMujeres) / TotalPersonas * 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }               
         public double PorcentajeHombres
         {
             get
             {
                 if (TotalPersonas != 0)
                 {
-                    return (double)AdultosHombres / TotalPersonas * 100;
+                    return ((double)AdultosHombres / TotalPersonas )* 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }        
+        public double PorcentajeMujeres
+        {
+            get
+            {
+                if (TotalPersonas != 0)
+                {
+                    return ((double)AdultosMujeres / TotalPersonas )* 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }       
+        public double PorcentajeMenoresHombres
+        {
+            get
+            {
+                if (TotalPersonas != 0)
+                {
+                    return (double)MenoresHombres / TotalPersonas * 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }       
+        public double PorcentajeMenoresMuejres
+        {
+            get
+            {
+                if (TotalPersonas != 0)
+                {
+                    return (double)MenoresMujeres / TotalPersonas * 1;
                 }
                 else
                 {
